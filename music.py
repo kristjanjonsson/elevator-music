81b81dd1fc66a8ebaa137b047f9da5d76de3d3d7
Halldór Eldjárn <halldor@plainvanillagames.com>
2014-08-14 13:39:30 +0000

    Added function to list directory

--- music.py
+++ music.py
@@ -1,18 +1,28 @@
 import pygame
+from os import listdir
+from os.path import isfile, join
+
+def get_sound_file_paths(mypath):
+    return [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 def main():
-	pygame.init()
-	mixer = pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
+    pygame.init()
+    mixer = pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=1024)
 
-	song = pygame.mixer.Sound('music/basketball.wav')
+    song = pygame.mixer.Sound('music/basketball.wav')
 
-	song.play()
+    song.play()
 
-	while True:
-		ev = pygame.event.poll()
-		if ev.type == pygame.QUIT:
-			break
+    running = True
 
-	pygame.quit()
+# Just for testing
+    print get_sound_file_paths('music')
 
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            else:
+                print(event.type)
+    pygame.quit()
 main()
