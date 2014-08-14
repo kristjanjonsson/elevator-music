cbaf0d90edac41bf3d57be3b0ba22434607ec74c
olafurorng <olafurorng@gmail.com>
2014-08-14 16:46:06 +0000

    check if it is raining

--- music.py
+++ music.py
@@ -1,6 +1,7 @@
 import pygame
 from os import listdir
 from os.path import isfile, join
+import requests
 
 def get_sound_file_paths(mypath):
     return [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
@@ -10,6 +11,7 @@ def main():
     mixer = pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=1024)
 
     song = pygame.mixer.Sound('music/basketball.wav')
+    # song = pygame.mixer.Sound('music/song1.mp3')
 
     song.play()
 
@@ -25,4 +27,23 @@ def main():
             else:
                 print(event.type)
     pygame.quit()
-main()
+
+def isRaining():
+    r = requests.get("http://apis.is/weather/observations/en?stations=1,422&time=1h&anytime=0")
+    
+    # get JSONObject
+    jsonResult = r.json()
+    results = jsonResult['results']
+    reykjavik = results[0]
+    
+    ## now we can get what information what we want
+    weatherNow = reykjavik['W']
+    rain = "Rain"
+    lightRain = "Light rain"
+    isRaining = weatherNow == rain or weatherNow == lightRain
+
+    #print isRaining
+    return isRaining
+
+isRaining = isRaining()
+print isRaining
